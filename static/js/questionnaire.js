648f9804a3c02123e9e6ae3747dd8151a8b5da9b
hachiilcane <hachi.il.cane@gmail.com>
2013-11-30 17:31:46 +0900

    全体満足度をスライダー入力にした

--- static/js/questionnaire.js
+++ static/js/questionnaire.js
@@ -10,5 +10,16 @@ jQuery(function ($) {
         alert("アンケート結果送信に失敗しました。");
       });
   });
+
+  $( "#slider_total_satisfaction" ).slider({
+    range: "min",
+    min: 1,
+    max: 5,
+    value: 3,
+    slide: function( event, ui ) {
+      $( "#total_satisfaction" ).val( ui.value );
+    }
+  });
+  $( "#total_satisfaction" ).val( $( "#slider_total_satisfaction" ).slider( "value" ) );
 });
 
