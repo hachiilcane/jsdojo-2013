b36e8aa8fc708a20637489bb9f72e5221e7e4861
ww24 <ww24gm@gmail.com>
2013-11-30 17:17:28 +0900

    calculate total

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,17 @@ module.exports = function () {
     if (! req.xhr)
       return res.send(400);
 
-    models.results.add(req.body, function (err) {
+    var result = {
+      times             : Number(req.body.times),
+      want_next         : req.body.want_next,
+      language          : req.body.language.toLowerCase(),
+      total_satisfaction: Number(req.body.total_satisfaction),
+      venue_satisfaction: Number(req.body.venue_satisfaction),
+      working_hours     : Number(req.body.working_hours),
+      work_satisfaction : Number(req.body.work_satisfaction)
+    };
+
+    models.results.add(result, function (err) {
       if (err)
         return res.send(500);
 
@@ -22,8 +32,67 @@ module.exports = function () {
 
   app.get("/result", function (req, res) {
     models.results.get(function (err, results) {
-      results = JSON.stringify(results);
-      res.render("result", {results: results});
+
+      var data = {
+        times             : {
+          1: 0,
+          2: 0
+        },
+        want_next         : {
+          yes: 0,
+          no : 0
+        },
+        language          : {
+          js    : 0,
+          ruby  : 0,
+          php   : 0,
+          java  : 0,
+          python: 0,
+          other : 0
+        },
+        total_satisfaction: {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0,
+          5: 0
+        },
+        venue_satisfaction: {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0,
+          5: 0
+        },
+        working_hours     : {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0,
+          5: 0
+        },
+        work_satisfaction : {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0,
+          5: 0
+        }
+      };
+
+      results.forEach(function (result) {
+        Object.keys(result).forEach(function (item) {
+          if (item === "id")
+            return;
+
+          data[item] || (data[item] = {});
+          data[item][result[item]] || (data[item][result[item]] = 0);
+          data[item][result[item]]++;
+        });
+      });
+
+      data = JSON.stringify(data);
+      res.render("result", {result: data});
     });
   });
 };
