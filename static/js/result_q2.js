a91dab86ebd895be778cd26dc0fe686749705d60
makies <makies@gmail.com>
2013-11-30 16:05:47 +0900

    結果画面 サンプルグラフを4つ入れてみた

---
+++ static/js/result_q2.js
@@ -0,0 +1,40 @@
+$(function() {
+
+//    var graph1;
+    var container = document.getElementById('editor-render-1');
+
+//    console.log('init');
+//    console.log(q2);
+//    console.log(q2.d1);
+
+    graph1 = Flotr.draw(container, [{
+        data: q2.d1,
+        label: 'Yes'
+    }, {
+        data: q2.d2,
+        label: 'No'
+    }], {
+        HtmlText: true,
+        grid: {
+            verticalLines: false,
+            horizontalLines: false
+        },
+        xaxis: {
+            showLabels: false
+        },
+        yaxis: {
+            showLabels: false
+        },
+        pie: {
+            show: true,
+            explode: 6
+        },
+        mouse: {
+            track: true
+        },
+        legend: {
+            position: 'se',
+            backgroundColor: '#D2E8FF'
+        }
+    });
+});
